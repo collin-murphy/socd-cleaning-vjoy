c6d354bc371c7ddc552ff5232d15b7f8bb519303
Collin <collinmuurphy@gmail.com>
2022-10-31 14:07:48 -0700

    windows working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#import pyvjoy
+import pyvjoy
 import keyboard
 
 keyboard.add_hotkey('space', lambda: print("space was pressed"))
-keyboard.wait
+keyboard.add_hotkey('w', lambda: print("w was pressed"))
+keyboard.wait()
